119b0808d40488637a66e1531508ad1635ca0a71
OJHA ARCHITHA <2303a52170@sru.edu.in>
2026-05-05 19:18:16 +0530

    Update CorsConfig.java

--- authify/src/main/java/in/ojha/com/authify/config/CorsConfig.java
+++ authify/src/main/java/in/ojha/com/authify/config/CorsConfig.java
@@ -1,12 +1,12 @@
 package in.ojha.com.authify.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 public class CorsConfig {
@@ -16,7 +16,9 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",
-                "http://127.0.0.1:3000"
+                "http://127.0.0.1:3000",
+                "https://authentication-system-n4jymc2ou-archithaojhas-projects.vercel.app",
+                "https://authentication-system-nine-delta.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "Origin", "X-Requested-With"));
@@ -27,4 +29,6 @@ public class CorsConfig {
         return source;
     }
 }
+
+
 
